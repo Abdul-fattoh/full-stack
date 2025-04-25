870d601a74a5d4b8f2c93c0e49433c472389a396
Abdulfattoh <a6du.xakim0v@gmail.com>
2025-04-25 18:30:20 +0500

    send mail and sign out added

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import { UserController } from '../controllers/auth.controller.js';
+import { jwtAuthGuard } from '../middlewares/auth.middleware.js';
+
+const router = Router();
+const controller = new UserController();
+
+router
+    .post('/admin', controller.createAdmin)
+    .post('/register', controller.registerUser)
+    .post('/login', controller.loginUser)
+    .post('/signOut', jwtAuthGuard, controller.signOutUser)
+    .get('/:id', jwtAuthGuard, controller.getUserById)
+    .get('/', jwtAuthGuard, controller.getAllUsers)
+    .patch('/:id', jwtAuthGuard, controller.updateUser)
+    .delete('/:id', jwtAuthGuard, controller.deleteUser);
+
+export { router as authRouter }
